de0dc7ca6034cd2449fa987cb8c8658b441a2445
gaurav876409 <gaurav876409@gmail.com>
2023-08-05 16:33:01 +0530

    update

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const usersRoute = require("./routes/user.js")
 const authMiddleware = require("./middleware/auth.js");
 const cors = require('cors');
 const app = express();
-const connectDb = async() => {
-   await mongoose.connect("mongodb+srv://gk05908:vera092gR8lyzRSj@cluster0.5seq6fn.mongodb.net/")
+const connectDb = async () => {
+    await mongoose.connect("mongodb+srv://gk05908:vera092gR8lyzRSj@cluster0.5seq6fn.mongodb.net/")
 }
 app.use(express.json());
 app.use(cors());
 connectDb()
-.then(() => console.log("mongodb connected successfully "))
-.catch(() => console.log("mongodb error"))
-app.use('/api/v1/event',authMiddleware, eventsRoute);
+    .then(() => console.log("mongodb connected successfully "))
+    .catch(() => console.log("mongodb error"))
+app.use('/api/v1/event', authMiddleware, eventsRoute);
 app.use('/api/v1/user', usersRoute);
 const portNo = 3001;
 app.listen(portNo, () => {
